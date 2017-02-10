4ed0cd5bfac59bc150afa607143baa87f271d367
Hebert Porto <hebertporto@gmail.com>
2017-02-10 17:50:15 -0200

    adicionando estrutura basica

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class App extends Component {
+
+
+  render() {
+    return (
+      <View>
+        <Text>
+           Iniciando o APP
+        </Text>
+      </View>
+    );
+  }
+}
+
+export default App;
